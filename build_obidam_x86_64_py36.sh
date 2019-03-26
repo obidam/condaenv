f4922d4f6815fe99076ea76478ecec989ce2f592
Guillaume Maze <code@guillaumemaze.org>
2019-03-26 12:55:52 +0100

    Update to py36 version

--- build_obidam_x86_64_py36.sh
+++ build_obidam_x86_64_py36.sh
@@ -25,29 +25,23 @@ conda create -n obidam36 python=3.6 anaconda
 source activate obidam36
 
 # Install packages:
-conda install -n obidam36 xarray dask netCDF4 bottleneck gsw\
- cartopy pynio pseudonetcdf intake intake-xarray rasterio intake-cmip intake-spark
+conda install -n obidam36 xarray dask netCDF4 bottleneck gsw cartopy pynio pseudonetcdf intake intake-xarray rasterio intake-cmip intake-spark
 
-conda install -n obidam36 seaborn hvplot python-graphviz\
- google-cloud-storage gcsfs parcels ffmpeg nodejs ipywidgets datashader pyviz hvplot geoviews
+conda install -n obidam36 seaborn hvplot python-graphviz google-cloud-storage gcsfs parcels ffmpeg nodejs ipywidgets datashader pyviz hvplot geoviews
 
-# conda install -n obidam36 -c conda-forge xarray dask netCDF4 bottleneck gsw
-# conda install -n obidam36 -c conda-forge cartopy pynio pseudonetcdf
-# conda install -n obidam36 dask-ml 
-# conda install -n obidam36 -c conda-forge intake intake-xarray rasterio intake-cmip intake-spark
+conda install -n obidam36 matplotlib-venn
 
-# conda install -n obidam36 -c conda-forge seaborn hvplot
-# conda install -n obidam36 google-cloud-storage gcsfs
-# conda install -n obidam36 -c conda-forge python-graphviz
-# 
-# conda install -n obidam36 parcels ffmpeg
+pip install --upgrade google-cloud-bigquery
+pip install papermill
+pip install -U pytest
+pip install motuclient  
 
+conda install -n obidam36 nodejs
 pip install sklearn-xarray dask-kubernetes dask_labextension
-# conda install -n obidam36 nodejs
 # conda install -n obidam36 -c conda-forge ipywidgets
 jupyter labextension install dask-labextension
 jupyter labextension install @pyviz/jupyterlab_pyviz
-jupyter labextension install toc github
+# jupyter labextension install toc github
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 
 # Add environment to Jupyter list of kernels:
