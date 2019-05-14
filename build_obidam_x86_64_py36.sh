78ee787b3484c4d232e44abfb5f84eecbecdc020
Guillaume Maze <code@guillaumemaze.org>
2019-05-14 22:11:02 +0200

    Update to py36 build

--- build_obidam_x86_64_py36.sh
+++ build_obidam_x86_64_py36.sh
@@ -19,6 +19,7 @@ conda update conda
 conda config --add channels conda-forge
 conda config --add channels anaconda-fusion
 conda config --add channels defaults
+conda config --add channels pyviz
 
 # Init the OBIDAM environment from anaconda:
 conda create -n obidam36 python=3.6 anaconda
@@ -30,11 +31,20 @@ conda install -n obidam36 xarray dask netCDF4 bottleneck gsw cartopy pynio pseud
 conda install -n obidam36 seaborn hvplot python-graphviz google-cloud-storage gcsfs parcels ffmpeg nodejs ipywidgets datashader pyviz hvplot geoviews
 
 conda install -n obidam36 matplotlib-venn
+conda install -n obidam36 nbsphinx
+
+conda install -n obidam36 mpi mpich openmpi mpi4py
+conda install -c anaconda gfortran_osx-64
+
+conda install -n obidam36 tensorflow keras
+pip install jupyter-tensorboard
+
 
 pip install --upgrade google-cloud-bigquery
 pip install papermill
 pip install -U pytest
 pip install motuclient  
+pip install sphinx_rtd_theme
 
 conda install -n obidam36 nodejs
 pip install sklearn-xarray dask-kubernetes dask_labextension
@@ -43,6 +53,7 @@ jupyter labextension install dask-labextension
 jupyter labextension install @pyviz/jupyterlab_pyviz
 # jupyter labextension install toc github
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install jupyterlab_tensorboard
 
 # Add environment to Jupyter list of kernels:
 python -m ipykernel install --user --name obidam36 --display-name "obidam36"
